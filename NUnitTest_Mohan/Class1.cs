8d42ff7235c140b06f49f754d1356fbd73a052c0
mokan <mokan@DESKTOP-TIKT6T7>
2024-06-05 22:20:02 -0400

    Add project files.

---
+++ NUnitTest_Mohan/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using NUnitDemonstration;
+
+namespace NUnitTestDemo 
+{
+    [TestFixture]
+    public class TriangleTest1
+    {
+        // Mohan Test
+        [Test]
+        public void ValidTriangle_Input60and60and60_OutputValidTriangle()
+        {
+            // Arrange
+            int firstAngle = 60;
+            int secondAngle = 60;
+            int thirdAngle = 60;
+            string expected = "The triangle is valid.";
+
+            // Act
+            string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+            // Assert
+            ClassicAssert.AreEqual(expected, actual);
+        }
+    }
+}
